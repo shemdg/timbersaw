e1c770aba7085be98bfada4a62dc6cf77373a217
shemdg <shemrdg@gmail.com>
2026-06-30 21:00:03 +0800

    migrated to angular

---
+++ src/app/pages/more/more.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../shared/navbar/navbar';
+
+@Component({
+  selector: 'app-builds',
+  imports: [Navbar],
+  templateUrl: './builds.html',
+  styleUrl: './builds.css'
+})
+export class Builds {}
